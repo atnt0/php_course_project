2b10b50c071fd45b7a16180148ced9f74baa0d1d
atnt0 <anry.tim@gmail.com>
2021-02-23 18:24:00 +0200

    Add set models

---
+++ app/Models/ProductPhotos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductPhotos extends Model
+{
+    use HasFactory;
+
+    public $table = "product_photos";
+
+}
